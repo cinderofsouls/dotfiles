867704435fece2a060512406debffa1480cda87a
Trevor Tilby <trevor.tilby@gmail.com>
2022-02-17 08:55:37 -0700

    Changed doom and kitty themes to dracula

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -6,9 +6,6 @@
 (setq doom-font (font-spec :family "Mononoki Nerd Font" :size 18)
       doom-variable-pitch-font (font-spec :family "Mononoki Nerd Font" :size 18))
 
-(setq doom-theme 'doom-palenight)
-
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Trevor Tilby"
@@ -30,7 +27,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;;(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-dracula)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
