c7c5ea321fa91b579494299c3a5e7fdda7eb4a69
Trevor Tilby <trevor.tilby@gmail.com>
2022-02-19 04:42:12 -0700

    Modified startup and xrandr refresh rate config

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -3,4 +3,8 @@
 ~/.screenlayout/layout.sh &
 nitrogen --restore &
 ~/.config/polybar/launch.sh &
+nvidia-settings --load-config-only
 picom --config ~/.config/picom/picom.conf -b
+
+com.leinardi.gwe &
+~/Applications/filen-setup_4dd6d00cddc72a4eca996c55623e22aa.AppImage &
