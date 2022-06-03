8091fc193f51ab1543400e5ec92ac41b9bbc7241
Trevor Tilby <trevor.tilby@gmail.com>
2022-06-02 23:54:42 -0600

    add kde polkit to i3 autostart

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -5,6 +5,7 @@ nitrogen --restore &
 ~/.config/polybar/launch.sh &
 nvidia-settings --load-config-only
 picom --config ~/.config/picom/picom.conf -b
+/usr/lib/polkit-kde-authentication-agent-1
 
 com.leinardi.gwe &
 ~/Applications/filen-setup_4dd6d00cddc72a4eca996c55623e22aa.AppImage &
