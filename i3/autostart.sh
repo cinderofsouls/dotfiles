65e728bce79f2680e51568ce7794aa80b4ce909e
Trevor Tilby <trevor.tilby@gmail.com>
2022-11-05 19:38:46 -0600

    update everything for reinstall

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
 ~/.screenlayout/layout.sh &
+echo "set screen layout"
 nitrogen --restore &
+echo "restored nitrogen wallpaper"
 ~/.config/polybar/launch.sh &
-nvidia-settings --load-config-only
+echo "launched polybar"
+#nvidia-settings --load-config-only
+#echo "loading nvidia settings"
 picom --config ~/.config/picom/picom.conf -b
-/usr/lib/polkit-kde-authentication-agent-1
+echo "launched picom"
+/usr/lib/polkit-kde-authentication-agent-1 &
+echo "launched kde polkit"
 
 #com.leinardi.gwe &
 gwe &
+echo "launched gwe"
 #~/Applications/filen-setup_4dd6d00cddc72a4eca996c55623e22aa.AppImage &
 #pcloud &
 megasync &
+echo "launched megasync"
