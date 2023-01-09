190d3e27f7a23a165ddf214f09c79e724d078b44
cndofx <19335833+cndofx@users.noreply.github.com>
2023-01-09 00:46:50 -0700

    configure doom emacs appearance

--- doom-emacs/config.el
+++ doom-emacs/config.el
@@ -32,11 +32,11 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;; (setq doom-theme 'doom-one)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+;; (setq display-line-numbers-type t)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -74,3 +74,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq doom-font (font-spec :family "FiraCode Nerd Font Mono" :size 18))
+(setq doom-theme 'catppuccin-macchiato)
+(setq display-line-numbers-type 'relative)
