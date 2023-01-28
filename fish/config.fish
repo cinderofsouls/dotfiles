fdc4e2722ac64e794519a8a5212d886596cba296
cndofx <19335833+cndofx@users.noreply.github.com>
2023-01-28 04:26:16 -0700

    set EDITOR env variable in fish to helix

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
+    set -x EDITOR helix
     fish_add_path ~/.emacs.d/bin
     starship init fish | source
     zoxide init fish | source
